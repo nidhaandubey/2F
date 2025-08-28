395e7a3951e7489e6bbb880d6671e72a67bb93b8
nidhaandubey <nidhaandubey2@gmail.com>
2025-08-29 01:12:52 +0530

    Routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,3 +3,20 @@ import passport from "passport";
 
 const router = Router();
 
+// Register Route
+router.post("/register",register);
+// Login Route
+router.post("/login", login);
+// Auth Status Route
+router.get("/status", authStatus)
+//logout route
+router.post("/logout", logout);
+
+// 2FA routes
+router.post("/2fa/setup",setup2FA);
+// Verify 2FA Route
+router.post("/2fa/verify",verify2FA);
+// reset 2FA Route
+router.post("/2fa/reset",reset2FA);
+
+export default router;
